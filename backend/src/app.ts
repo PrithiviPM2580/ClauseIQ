58b213b79d89155c8aefd0d91c01bc774f5ab338
PrithiviPM2580 <prithivipemimagar>
2025-10-11 02:46:21 +0545

    feat: implement CI/CD workflows and enhance backend with Morgan logging and type definitions

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,27 @@
 import express, { type Express } from 'express';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import corsOptions from '@/lib/cors.lib';
+import compressionMiddleware from '@/middleware/compression.middleware';
+import logger from './lib/logger.lib';
+import globalErrorHandler from './middleware/globalErrorHandler.middleware';
 
 const app: Express = express();
+
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(compressionMiddleware);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
+app.use(globalErrorHandler);
 
 export default app;
