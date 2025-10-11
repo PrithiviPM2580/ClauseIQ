850191bef696800876a67fcbbe310fd4ac1b6882
PrithiviPM2580 <prithivipemimagar>
2025-10-11 13:29:36 +0545

    feat: initialize frontend with React, TypeScript, Vite, and Tailwind CSS with linting and prettier for format

---
+++ frontend/eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      ...prettier.configs.recommended.rules, // ✅ replaces "plugin:prettier/recommended"
+      'prettier/prettier': 'warn', // show formatting issues as warnings
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    ignores: ['node_modules/', 'dist/', 'coverage/', '.github/'],
+  },
+]);
